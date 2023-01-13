a061b9174a1363fcf851e99dfa171000520d90ee
liupoyip <liupoyip@gmail.com>
2023-01-13 16:55:25 +0800

    LGTM!!

--- function/question_05/myModule.py
+++ function/question_05/myModule.py
@@ -4,10 +4,10 @@ def ninja(code):
         raise BaseException('ninja say: 87')
     if len(code) != 3:
         raise BaseException('ninja say: 87分不能再高了')
-    if code[0] != int(0x16):
+    if code[0] != int(0xFF):
         raise BaseException('ninja say: You are not a pro ninja, but you do better!!')
     if code[1] != 'ninja':
         raise BaseException('ninja say: You are a clerkship ninja!!')
-    if code[2] != 'I\'m ninja!1':
+    if code[2] != 'I\'m ninja!':
         raise BaseException('ninja say: It\'s very close!! あと一歩')
     print('おめでとうございます！　忍者なれました！')
