ded013da5604a7bec82b7f1888a41539d1dde2f2
liupoyip <liupoyip@gmail.com>
2023-01-30 16:57:09 +0800

    LGTM!!

--- class_basic/ex_01/main.py
+++ class_basic/ex_01/main.py
@@ -1,5 +1,5 @@
 # %%
-from ex_01_basic import Car
+from mymodule import Car
 
 altis_2021 = Car('altis_2021', '18 inch', 'luxury seet', 'fix speed mode ok')
 altis_2023 = Car('altis_2023', '17 inch', 'pankin seet', 'fix speed mode ng')
